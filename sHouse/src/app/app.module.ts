c9cd24314dd2710de1b90409ee9e57736daa6557
angel_laptop <avictoria@MacBook-Pro-2.fios-router.home>
2017-07-09 14:28:43 -0400

    Adding new componen and Routes

--- sHouse/src/app/app.module.ts
+++ sHouse/src/app/app.module.ts
@@ -4,15 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SonyComponent } from './sony/sony.component';
+import { LgComponent } from './lg/lg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SonyComponent,
+    LgComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+    {path:'sony', component: SonyComponent},
+    {path:'lg', component: LgComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
